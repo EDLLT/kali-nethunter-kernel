0b19be8ec720bd8e66580f75537bda6f469af3d2
EDLLT <47005435+EDLLT@users.noreply.github.com>
2023-04-06 19:53:55 +0300

    Modified anykernel.sh to work with t03g

--- anykernel3/anykernel.sh
+++ anykernel3/anykernel.sh
@@ -6,13 +6,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NetHunter Kernel for the OnePlus 7 Pro
+kernel.string=NetHunter Kernel for Samsung Galaxy Note 2 by EDllT
 do.devicecheck=1
 do.modules=1
 do.systemless=0 #Never use this for NetHunter kernels as it prevents us from writing to /lib/modules
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=OnePlus7Pro
+device.name1=t03g
 device.name2=guacamole
 device.name3=OnePlus 7 Pro
 device.name4=Guacamole
@@ -31,8 +31,8 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=1;
+block=/dev/block/bootdevice/by-name/BOOT;
+is_slot_device=0;
 ramdisk_compression=auto;
 
 ## AnyKernel methods (DO NOT CHANGE)
